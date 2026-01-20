f020c01004788f41f0b58415b4eea729c606f55f
xvekii <vedran.dautanec@gmail.com>
2026-01-20 21:25:13 +0100

    Add initial API test

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+console.log("oi!");
+
+async function getWeather() {
+  const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%C4%90ur%C4%91evac?unitGroup=us&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=H9CXBS2KVNGA3AW4L8PF8PGK4&contentType=json');
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeather();
