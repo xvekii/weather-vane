461c538585f03b787119c06377d56a8444a09fba
xvekii <vedran.dautanec@gmail.com>
2026-01-22 08:40:41 +0100

    Add const URL

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import "./styles.css";
 
 // Add search form and location variable
 // variable.toLowerCase()
-
+const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%C4%90ur%C4%91evac?unitGroup=metric&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=H9CXBS2KVNGA3AW4L8PF8PGK4&contentType=json';
 // Implement location variable inside link, otherwise default Shanghai
 // Return data 
 async function getWeather() {
-  const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%C4%90ur%C4%91evac?unitGroup=us&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=H9CXBS2KVNGA3AW4L8PF8PGK4&contentType=json');
+  const response = await fetch(URL);
   const weatherData = await response.json();
   console.log(weatherData);
 }
 
-// getWeather();
+getWeather();
