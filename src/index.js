514c0ca2e2f946a5e8b6a15961a7e34c2ad7e947
xvekii <vedran.dautanec@gmail.com>
2026-01-26 20:23:40 +0100

    Remove spaces and extra code, add hide fn, remove showLoader from uneccessary spots, add empty location check, add locationsDiv eventListener, edit getWeather

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ function hideLoader() {
   loader.classList.remove("is-visible");
 }
 
+// Add one function show?
+
 const apiKey = "H9CXBS2KVNGA3AW4L8PF8PGK4";
 const locAPIKey = "bd15706d81c640a398a191135262501";
 
@@ -25,40 +27,39 @@ const defaultURL = `https://weather.visualcrossing.com/VisualCrossingWebServices
 // Implement location variable inside link, otherwise default Shanghai
 // Return data 
 
-// if backspace pressed, repopulate results
-
 input.addEventListener("input", () => {
   if (!input.value.trim()) {
-    locationsDiv.replaceChildren();
-    locationsDiv.classList.remove("location");
+    hide(locationsDiv, "location");
   }
-  
   const query = input.value.trim();
   
   if(query < 2) {
     return;
   }
-  
   searchLocations(query);
 });
 
 async function searchLocations(query) {
   try {
-    showLoader();
     const res = await fetch(`http://api.weatherapi.com/v1/search.json?key=${locAPIKey}&q=${encodeURIComponent(query)}`);
     const locations = await res.json();
+
     showLocations(locations);
     console.log(locations);
-    hideLoader();
   } catch (err) {
     // Add N/A
-    hideLoader();
     console.log(err);
   }
 }
 
 function showLocations(locs) {
   locationsDiv.replaceChildren();
+  
+  if (locs.length === 0) {
+    locationsDiv.classList.remove("location");
+    return;
+  }
+
   locs.forEach(loc => {
     const span = document.createElement("span");
     span.textContent = `${loc.name}, ${loc.country}`;
@@ -68,34 +69,38 @@ function showLocations(locs) {
   
   form.appendChild(locationsDiv);
   locationsDiv.classList.add("location");
-
 }
 
-function getLocation() {
-  let inputVal = input.value;
-  const loc = encodeURIComponent(inputVal);
-  return loc.toLowerCase();
-}
+// Get location value on click, pass to getWeather
+locationsDiv.addEventListener("mousedown", (e) => {
+  const span = e.target.closest(".loc-span");
+  if (!span) return;
+  
+  e.preventDefault();
+  
+  const location = span.textContent.trim();
+  getWeather(location);
+  input.value = "";
+  
+  hide(locationsDiv, "location");
+});
 
-// Refactor to get value on click
-// input.addEventListener("mousedown", (e) => {
-//   if (e.key === "Enter") {
-//     e.preventDefault();
-//     const inputLoc = getLocation();
-//     getWeather(inputLoc);
-//   }
-// });
+function hide(el, className) {
+  el.replaceChildren();
+  el.classList.remove(className);
+}
 
 
 async function getWeather(loc) {
   try {
     showLoader();
-    // const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=metric&elements=add%3Atimezone%2Cadd%3Atzoffset%2Cremove%3Acloudcover%2Cremove%3Amoonphase%2Cremove%3Apressure%2Cremove%3Astations&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=${apiKey}&contentType=json`;
-    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=metric&elements=add%3Aaddress%2Cadd%3Alatitude%2Cadd%3Alongitude%2Cadd%3AresolvedAddress%2Cadd%3Atimezone%2Cadd%3Atzoffset%2Cremove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Amoonphase%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asevererisk%2Cremove%3Asnow%2Cremove%3Asnowdepth%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex%2Cremove%3Avisibility%2Cremove%3Awinddir%2Cremove%3Awindgust&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=${apiKey}&contentType=json`;
+    const encodedLoc = encodeURIComponent(loc);
+    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedLoc}?unitGroup=metric&elements=add%3Aaddress%2Cadd%3Alatitude%2Cadd%3Alongitude%2Cadd%3AresolvedAddress%2Cadd%3Atimezone%2Cadd%3Atzoffset%2Cremove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Amoonphase%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asevererisk%2Cremove%3Asnow%2Cremove%3Asnowdepth%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex%2Cremove%3Avisibility%2Cremove%3Awinddir%2Cremove%3Awindgust&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=${apiKey}&contentType=json`;
     const response = await fetch(URL);
     const weatherData = await response.json();
     console.log(weatherData);
     console.log(weatherData.address);
+    // Add fn that takes in data and hydrates fields
     hideLoader();
   } catch (err) {
     // Add N/A
@@ -103,7 +108,3 @@ async function getWeather(loc) {
     console.log(err);
   }
 }
-
-
-
-// getWeather();
