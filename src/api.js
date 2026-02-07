4a976814436d1c5be262df79069fab45e4775404
xvekii <vedran.dautanec@gmail.com>
2026-02-07 10:39:57 +0100

    Move API related code to api.js, refactor functions accordingly

---
+++ src/api.js
@@ -0,0 +1,37 @@
+const WEATHER_API_KEY = "H9CXBS2KVNGA3AW4L8PF8PGK4";
+const LOCATION_API_KEY = "bd15706d81c640a398a191135262501";
+
+export async function searchLocations(query) {
+  try {
+    const res = await fetch(`http://api.weatherapi.com/v1/search.json?key=${LOCATION_API_KEY}&q=${encodeURIComponent(query)}`);
+    
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const locations = await res.json();
+    return locations;
+
+  } catch (err) {
+    // Add N/A
+    console.error('Error fetching locations:', err);
+  }
+}
+
+export async function getWeather(loc) {
+  try {
+    const encodedLoc = encodeURIComponent(loc);
+    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedLoc}?unitGroup=metric&elements=add%3Aaddress%2Cadd%3Alatitude%2Cadd%3Alongitude%2Cadd%3AresolvedAddress%2Cadd%3Atimezone%2Cadd%3Atzoffset%2Cremove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Amoonphase%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asevererisk%2Cremove%3Asnow%2Cremove%3Asnowdepth%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex%2Cremove%3Avisibility%2Cremove%3Awinddir%2Cremove%3Awindgust&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=${WEATHER_API_KEY}&contentType=json`;
+    const response = await fetch(URL);
+    
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const weatherData = await response.json();
+    return weatherData;
+
+  } catch (err) {
+    // Add N/A
+    console.error('Error fetching weather:', err);
+    throw err;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -8,11 +8,8 @@ import {
   fillSecondaryCardWeather,
   showLocations,
   setWeatherIcon,
-
 } from "./ui.js";
-
-const WEATHER_API_KEY = "H9CXBS2KVNGA3AW4L8PF8PGK4";
-const LOCATION_API_KEY = "bd15706d81c640a398a191135262501";
+import { searchLocations, getWeather } from "./api.js";
 
 const form = document.querySelector("#searchForm");
 const input = document.querySelector("#query");
@@ -35,7 +32,7 @@ const hourlyPrecip = document.querySelector(".hourly-precip");
 
 // Add one function show?
 
-input.addEventListener("input", () => {
+input.addEventListener("input", async () => {
   if (!input.value.trim()) {
     hide(locationsDiv, "location");
     return;
@@ -45,21 +42,10 @@ input.addEventListener("input", () => {
   if(query < 2) {
     return;
   }
-  searchLocations(query);
+  const locations = await searchLocations(query);
+  showLocations({ locations, locationsDiv, form });
+  console.log(locations);
 });
-
-async function searchLocations(query) {
-  try {
-    const res = await fetch(`http://api.weatherapi.com/v1/search.json?key=${LOCATION_API_KEY}&q=${encodeURIComponent(query)}`);
-    const locations = await res.json();
-
-    showLocations({ locations, locationsDiv, form });
-    console.log(locations);
-  } catch (err) {
-    // Add N/A
-    console.log(err);
-  }
-}
 
 // Get location value on click, pass to getWeather
 locationsDiv.addEventListener("mousedown", (e) => {
@@ -69,25 +55,17 @@ locationsDiv.addEventListener("mousedown", (e) => {
   e.preventDefault();
   
   const location = span.textContent.trim();
-  getWeather(location);
+  handleGetWeather(location);
   input.value = "";
   
   hide(locationsDiv, "location");
 });
 
-async function getWeather(loc) {
+async function handleGetWeather(loc) {
   try {
     showLoader(formLoader);
-    const encodedLoc = encodeURIComponent(loc);
-    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedLoc}?unitGroup=metric&elements=add%3Aaddress%2Cadd%3Alatitude%2Cadd%3Alongitude%2Cadd%3AresolvedAddress%2Cadd%3Atimezone%2Cadd%3Atzoffset%2Cremove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Amoonphase%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asevererisk%2Cremove%3Asnow%2Cremove%3Asnowdepth%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex%2Cremove%3Avisibility%2Cremove%3Awinddir%2Cremove%3Awindgust&include=days%2Chours%2Ccurrent%2Calerts%2Cevents&key=${WEATHER_API_KEY}&contentType=json`;
-    const response = await fetch(URL);
+    const weatherData = await getWeather(loc);
     
-    if (!response.ok) {
-      hideLoader(formLoader);
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const weatherData = await response.json();
     console.log(weatherData);
     setWeatherData(weatherData);
 
@@ -97,17 +75,14 @@ async function getWeather(loc) {
     if (currentWeatherIcon) {
       setWeatherIcon(currentWeatherIcon);
     } else {
-      // Add N/A
       console.log("Icon error");
     }
     hideLoader(formLoader);
   } catch (err) {
-    // Add N/A
     hideLoader(formLoader);
     console.log(err);
   }
 }
-
 
 function setWeatherData(data) {
   fillSearchInput({ locationStr: data.address, input }); 
@@ -214,4 +189,4 @@ function createRow() {
 
 
 
-getWeather("Đurđevac, Croácia");
+handleGetWeather("Đurđevac, Croácia");
