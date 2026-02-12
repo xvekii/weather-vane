fed885593e067e1ac7afef4d6c4cd6c976fc303d
xvekii <vedran.dautanec@gmail.com>
2026-02-12 19:53:44 +0100

    Add miniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,67 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+module.exports = (_, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    mode: isProd ? "production" : "development",
+
+    entry: "./src/index.js",
+
+    output: {
+      filename: isProd ? "[name].[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+
+    devtool: isProd ? "source-map" : "eval-source-map",
+
+    devServer: {
+      watchFiles: ["./src/**/*"],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
+      ...(isProd
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "[name].[contenthash].css",
+            }),
+          ]
+        : []),
     ],
-  },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
